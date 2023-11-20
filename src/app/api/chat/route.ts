02a65c031dda4cff8a8fa3098c0685fa08bb0f3a
ayushkoli772 <113872531+ayushkoli772@users.noreply.github.com>
2023-11-20 18:07:50 +0530

    remove SSE route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -11,13 +11,22 @@ const humanloop = new Humanloop({
   apiKey: process.env.HUMANLOOP_API_KEY,
 });
 
+// export async function POST(req: Request): Promise<Response> {
+//   const messages: ChatMessage[] = (await req.json()) as ChatMessage[];
+
+//   const response = await humanloop.chatDeployedStream({
+//     project: "mini-project",
+//     messages,
+//   });
 export async function POST(req: Request): Promise<Response> {
   const messages: ChatMessage[] = (await req.json()) as ChatMessage[];
+  console.log(messages);
 
-  const response = await humanloop.chatDeployedStream({
+  const response = await humanloop.chatDeployed({
     project: "mini-project",
     messages,
   });
 
-  return new Response(response.data);
+  // return new Response(response.data);
+  return new Response(JSON.stringify(response.data.data[0].output));
 }
